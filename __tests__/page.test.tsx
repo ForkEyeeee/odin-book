c5ca8b0881feafad6f1cd613b91645283efe09df
ForkEyeee <shawncarter123456@gmail.com>
2023-10-30 12:06:10 -0400

    added test for HomePage

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,12 +1,19 @@
-import { render, screen } from "@testing-library/react";
-import Home from "@/app/page";
-import "@testing-library/jest-dom";
+import { render, screen } from '@testing-library/react';
+import HomePage from '@/app/components/HomePage';
+import '@testing-library/jest-dom';
 
-describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
-
-    const text = screen.getByText("Get started by editing");
-    expect(text).toBeInTheDocument();
+describe('Home', () => {
+  it('renders a heading', () => {
+    const data = [
+      {
+        id: 219,
+        content: 'This is a detailed post by User 1, post number 1.',
+        imageUrl: null,
+        authorId: 89,
+        createdAt: new Date(),
+      },
+    ];
+    render(<HomePage data={data} />);
+    expect(screen.getByText('This is a detailed post by User 1, post number 1.')).toBeVisible();
   });
 });
