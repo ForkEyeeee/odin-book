4fff374cf981888b2cf6b75a4ebd91335b583e55
ForkEyeee <shawncarter123456@gmail.com>
2023-11-25 18:23:01 -0500

    removed template code from Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,17 +1,16 @@
 'use client';
-
 import {
   Box,
-  chakra,
   Container,
   Stack,
   Text,
   useColorModeValue,
-  VisuallyHidden,
+  HStack,
+  IconButton,
+  LinkBox,
 } from '@chakra-ui/react';
-import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
-import { ReactNode } from 'react';
-import ScrollToTop from './ScrollToTop';
+import { FaGithub } from 'react-icons/fa';
+import Link from 'next/link';
 
 const Logo = (props: any) => {
   return (
@@ -28,40 +27,6 @@ const Logo = (props: any) => {
   );
 };
 
-const SocialButton = ({
-  children,
-  label,
-  href,
-}: {
-  children: ReactNode;
-  label: string;
-  href: string;
-}) => {
-  return (
-    <>
-      <chakra.button
-        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-        rounded={'full'}
-        w={8}
-        h={8}
-        cursor={'pointer'}
-        as={'a'}
-        href={href}
-        display={'inline-flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        transition={'background 0.3s ease'}
-        _hover={{
-          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-        }}
-      >
-        <VisuallyHidden>{label}</VisuallyHidden>
-        {children}
-      </chakra.button>
-    </>
-  );
-};
-
 export default function SmallWithLogoLeft() {
   return (
     <Box
@@ -72,24 +37,31 @@ export default function SmallWithLogoLeft() {
         as={Stack}
         maxW={'6xl'}
         py={4}
-        direction={{ base: 'column', md: 'row' }}
+        direction={{ base: 'column', sm: 'row' }}
         spacing={4}
         justify={{ base: 'center', md: 'space-between' }}
         align={{ base: 'center', md: 'center' }}
       >
         <Logo />
-        <Text>© 2022 Chakra Templates. All rights reserved</Text>
-        <Stack direction={'row'} spacing={6}>
-          <SocialButton label={'Twitter'} href={'#'}>
-            <FaTwitter />
-          </SocialButton>
-          <SocialButton label={'YouTube'} href={'#'}>
-            <FaYoutube />
-          </SocialButton>
-          <SocialButton label={'Instagram'} href={'#'}>
-            <FaInstagram />
-          </SocialButton>
-        </Stack>
+        <HStack spacing={6}>
+          <Text fontSize={'xl'}>ForkEyeee </Text>
+          <Box>
+            <LinkBox>
+              <Link href={'https://github.com/ForkEyeee'} target="_blank">
+                <IconButton
+                  icon={<FaGithub size="1.5em" />}
+                  aria-label="Github"
+                  variant="ghost"
+                  size="lg"
+                  _hover={{
+                    bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+                  }}
+                />
+              </Link>
+            </LinkBox>
+          </Box>
+        </HStack>
+        <Text>Social Media App</Text>
       </Container>
     </Box>
   );
