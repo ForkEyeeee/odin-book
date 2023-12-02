a42662b089f9a55a70d9666f2d86e1a6de95649b
ForkEyeee <shawncarter123456@gmail.com>
2023-12-02 03:43:34 -0500

    refactored profile page.tsx to fix build errors. preparing for deployment

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,7 +2,7 @@ import { getPostTime, getProfile, getUserId, getUserPosts } from '../lib/actions
 import Profile from '../components/Profile';
 import PaginationContainer from '../components/PaginationContainer';
 import NoDataFound from '../components/NoDataFound';
-import { Post, Profile as UserProfile } from '../lib/definitions';
+import { Profile as UserProfile } from '../lib/definitions';
 
 export default async function Page({
   searchParams,
@@ -12,41 +12,35 @@ export default async function Page({
     page?: string;
   };
 }) {
-  const currentUserId = await getUserId();
-  const userId = Number(searchParams?.userid);
-  const page = Number(searchParams?.page) || 1;
-
   try {
-    const promise1 = getProfile(userId);
-    const promise2 = getUserPosts(page, userId);
+    const profileUserId = Number(searchParams?.userid);
+    const page = Number(searchParams?.page) || 1;
+    const [profileData, userPostsData, userId] = await Promise.all([
+      getProfile(profileUserId),
+      getUserPosts(page, profileUserId),
+      getUserId(),
+    ]);
 
-    const allPromise = Promise.all([promise1, promise2]);
+    if (!userPostsData?.userPosts || !userPostsData?.postsCount) {
+      throw new Error('No data found');
+    }
 
-    const [profile, { userPosts, postsCount }] = await allPromise;
-
-    if (userPosts === undefined || postsCount === undefined) throw new Error();
-
-    await Promise.all(
-      userPosts.map(async (post: Post) => {
-        try {
-          const test = await getPostTime(post.createdAt);
-          post.postTime = test;
-        } catch (error) {
-          return { message: 'Error calculating post time' };
-        }
-      })
+    const userPosts = await Promise.all(
+      userPostsData.userPosts.map(async post => ({
+        ...post,
+        postTime: await getPostTime(post.createdAt).catch(() => 'Error calculating post time'),
+      }))
     );
 
-    const isAuthor = currentUserId === (profile as UserProfile).userId;
+    const isAuthor = profileUserId === userId;
 
     return (
       <>
-        <Profile profile={profile as UserProfile} posts={userPosts} isAuthor={isAuthor} />
-        <PaginationContainer page={page} timelinePostsCount={postsCount} />
+        <Profile profile={profileData as UserProfile} posts={userPosts} isAuthor={isAuthor} />
+        <PaginationContainer page={page} timelinePostsCount={userPostsData.postsCount} />
       </>
     );
   } catch (error) {
-    console.error(error);
     return <NoDataFound />;
   }
 }
