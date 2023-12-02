106acc477785c2839c7165e316de0ce2e6a20e62
ForkEyeee <shawncarter123456@gmail.com>
2023-12-02 03:54:54 -0500

    adjusted authoptions, preparing for deployment

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -34,6 +34,7 @@ export const authOptions: AuthOptions = {
       },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async signIn({ user, account, profile }) {
       try {
