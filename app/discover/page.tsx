ab7f8fc41fe34d53a0d0ce2a8653d98ae1cc1a28
ForkEyeee <shawncarter123456@gmail.com>
2023-12-02 22:37:03 -0500

    fixed time calculation for discover

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,10 +1,11 @@
 import TimeLineTabs from '../components/TimeLineTabs';
 import PaginationContainer from '../components/PaginationContainer';
-import { getPosts } from '@/app/lib/actions';
+import { getPosts, getPostTime } from '@/app/lib/actions';
 import NoDataFound from '../components/NoDataFound';
 import PostList from '../components/PostList';
 import NoTimeLine from '../components/NoTimeLine';
 import { Box } from '@chakra-ui/react';
+import { Post } from '../lib/definitions';
 
 export default async function Page({
   searchParams,
@@ -19,6 +20,17 @@ export default async function Page({
 
     const { otherTimeLinePosts, userId, otherTimelinePostsCount } = await getPosts(page);
     if (userId === undefined) return <NoDataFound />;
+
+    await Promise.allSettled(
+      otherTimeLinePosts.map(async (post: Post) => {
+        try {
+          const test = await getPostTime(post.createdAt);
+          post.postTime = test;
+        } catch (error) {
+          return { message: 'Error calculating post time' };
+        }
+      })
+    );
 
     return (
       <Box mt={0}>
